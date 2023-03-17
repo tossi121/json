e7d806d192a9c0b824acd73039bc48e6f2c6a2e8
tosif deora <tosif.geekologix@gmail.com>
2023-03-17 18:48:14 +0530

    fixes

--- index.js
+++ index.js
@@ -70,14 +70,60 @@ const server = http.createServer((req, res) => {
   } else {
     res.end(`
       <html>
+      <head>
+      <style>
+        body {
+          background: url("https://images.unsplash.com/photo-1507238691740-187a5b1d37b8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=755&q=80") no-repeat;
+          height:100vh;
+          background-size:cover;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          background-position: center;
+        }
+        .box{
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+        }
+        .input-box {
+          border: none;
+          border-radius: 5px;
+          background-color: #fff;
+          padding: 10px;
+          font-size: 16px;
+          box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.1);
+          margin-bottom: 10px;
+          width: 300px;
+        }
+        .input-box:focus {
+          outline: none;
+          box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3);
+        }
+        .submit-btn {
+          background-color: #4CAF50;
+          color: #fff;
+          border: none;
+          border-radius: 5px;
+          padding: 10px 20px;
+          font-size: 16px;
+          box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.1);
+          cursor: pointer;
+          transition: all 0.2s ease-in-out;
+        }
+        .submit-btn:hover {
+          background-color: #3e8e41;
+          box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3);
+        }
+      </style>
+    </head>
         <body>
-          <form method="POST" id="myForm">
-            <label for="name">Name:</label>
-            <input type="text" id="name" name="name"><br>
+          <form method="POST" id="myForm" class="box">
+            <input type="text" id="name" name="name" class="input-box">
             <button onClick="this.disabled=true;
             this.value='Sending…';
             this.form.submit();"
-             id="submitBtn"> Ok </button>
+             id="submitBtn" class="submit-btn"> Get Data </button>
             </form>
         </body>
       </html>
